1044eb7e104f49118a9d6fa0566ceaf3d4ca424f
Syota Nishimura <syota4723@gmail.com>
2023-08-16 21:32:39 +0900

    :lipstick: アカウント登録用画面の作成

--- app/page.tsx
+++ app/page.tsx
@@ -12,24 +12,19 @@ export default function Home() {
           <div className="flex flex-col text-center w-full my-12">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">ログイン</h1>
           </div>
-          <div className="lg:w-1/2 md:w-2/3 mx-auto">
+          {/* ログイン用画面 */}
+          {/* <div className="lg:w-1/2 md:w-2/3 mx-auto">
             <div className="flex flex-col -m-2 items-center">
               <div className="p-2 w-3/4">
                 <div className="relative">
-                  <label htmlFor="name" className="leading-7 text-sm text-gray-600">メールアドレス</label>
-                  <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                </div>
-              </div>
-              <div className="p-2 w-3/4">
-                <div className="relative">
-                  <label htmlFor="email" className="leading-7 text-sm text-gray-600">パスワード</label>
+                  <label htmlFor="email" className="leading-7 text-sm text-gray-600">メールアドレス</label>
                   <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
               </div>
               <div className="p-2 w-3/4">
                 <div className="relative">
-                  <label htmlFor="email" className="leading-7 text-sm text-gray-600">パスワード確認用</label>
-                  <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                  <label htmlFor="password" className="leading-7 text-sm text-gray-600">パスワード</label>
+                  <input type="password" id="password" name="password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
               </div>
               <div className="p-2 w-full">
@@ -39,8 +34,48 @@ export default function Home() {
                 <Link href={"/"} className="text-gray-400 text-sm">アカウントをお持ちではない方はこちらから</Link>
                 <Link href={"/"} className="text-gray-400 text-sm">パスワードを忘れた</Link>
               </div>
+            </div> */}
+          {/* ログイン用画面 */}
+          {/* サインイン用画面 */}
+          <div className="lg:w-1/2 md:w-2/3 mx-auto">
+            <div className="flex flex-col -m-2 items-center">
+              <div className="p-2 w-3/4 md:flex md:justify-between">
+                <div className="relative md:w-2/5">
+                  <label htmlFor="firstName" className="leading-7 text-sm text-gray-600">姓</label>
+                  <input type="text" id="firstName" name="firstName" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+                <div className="relative md:w-2/5">
+                  <label htmlFor="lastName" className="leading-7 text-sm text-gray-600">名</label>
+                  <input type="text" id="lastName" name="lastName" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-3/4">
+                <div className="relative">
+                  <label htmlFor="email" className="leading-7 text-sm text-gray-600">メールアドレス</label>
+                  <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-3/4">
+                <div className="relative">
+                  <label htmlFor="password" className="leading-7 text-sm text-gray-600">パスワード</label>
+                  <input type="password" id="password" name="password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-3/4">
+                <div className="relative">
+                  <label htmlFor="passwordConfirmation" className="leading-7 text-sm text-gray-600">パスワード確認用</label>
+                  <input type="password" id="passwordConfirmation" name="passwordConfirmation" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">サインイン</button>
+              </div>
+              <div className='flex flex-col p-2'>
+                <Link href={"/"} className="text-gray-400 text-sm">既にアカウントをお持ちの場合はこちらから</Link>
+              </div>
             </div>
           </div>
+          {/* サインイン用画面 */}
         </div>
       </section >
     </>
