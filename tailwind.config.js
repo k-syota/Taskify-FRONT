9e9e60926568862d9af070affd92a5f4eb18567a
Syota Nishimura <syota4723@gmail.com>
2023-09-09 15:11:11 +0900

    :lipstick: tailwindcssのカラー設定を修正
    カラーの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "primary-color": "#192f60",
+        "secondary-color": "#26499d"
+      }
     },
   },
   plugins: [],
